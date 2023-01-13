83afec37a9a1128fa8ed17846f6710d450777984
Rakibhasaniue <rakibhasanoyoniu@gmail.com>
2023-01-13 23:27:20 +0600

    dynamic card details added

--- src/Pages/Home/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import NewsCard from '../../../../Shared/NewsCard/NewsCard';
 
 const Home = () => {
-    const news = useLoaderData();
+    const allNews = useLoaderData();
     return (
         <div >
-            <h2>Today News : {news.length}</h2>
+            <h2>Today News : {allNews.length}</h2>
+            {
+                allNews.map(news => <NewsCard
+                    key={news._id}
+                    news={news}
+                ></NewsCard>)
+            }
         </div>
     );
 };
